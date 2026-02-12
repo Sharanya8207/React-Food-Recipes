5a30047699df64ab30ec34edf8929523be43057b
Sharanya8207 <bhumasharanya28@gmail.com>
2026-02-12 12:21:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import SearchView from "./components/SearchView";
 import CuisineBar from "./components/Cuisine";
 import HomeView from "./components/HomeView";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 
 const API_URL = "https://www.themealdb.com/api/json/v1/1/";
 
